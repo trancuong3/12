b9f59b889d0cb5097c8017064a4c0b018c47907b
trancuong3 <161544695+trancuong3@users.noreply.github.com>
2024-03-13 19:12:43 +0700

    d

---
+++ d/hii.js
@@ -0,0 +1,8 @@
+var a = prompt("Nhập số thứ nhất:");
+var b = prompt("Nhập số thứ hai:");
+
+if (a % b === 0) {
+    alert(a + " là bội số của " + b);
+} else {
+    alert(a + " không phải là bội số của " + b);
+}
